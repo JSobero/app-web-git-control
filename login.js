1101c94faeecfa59daff90e1b0fc64492ae24ae7
Yaret Blas <yaretblas@gmail.com>
2025-09-25 11:09:14 -0500

    agregar generacion de token temporal en login

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
+// develop: generar token (archivo login.js)
 function login(user){
-  if(!user){ 
-    return false; 
-  }
+  if(!user){ return false; }
+  // generate token
+  user.token = generateToken(user); // suponiendo que existe generateToken
   return true;
 }
