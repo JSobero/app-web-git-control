fa8f50fa44e5484189fda06875c4dbf9aaa65a12
Yaret Blas <yaretblas@gmail.com>
2025-09-25 11:07:49 -0500

    agregar validacion de email login

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
+// feature: validacion de email (archivo login.js)
 function login(user){
-  if(!user){ 
-    return false; 
-  }
+  if(!user){ return false; }
+  // nueva validacion: email
+  if(!/^\S+@\S+\.\S+$/.test(user.email)){ return false; }
   return true;
 }
